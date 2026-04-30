42184b49a07b5091488d4730e7d4ddc2e5878b9b
Irakoze Armel <iraarmel90@gmail.com>
2026-04-30 19:57:25 +0200

    FigCaption and Social Media

--- instruction.md
+++ instruction.md
@@ -3,3 +3,4 @@ The website should include the following information:
 - Contact information of the company (tel: 79 49 31 21; email: buildingrelatedservicesbrst@gmail.com)
 - how to contact them (if possible, a link Whatsapp icon)
 - project already done by the company
+- Create a Youtube Channel with the email of the company and post videos
--- script.js
+++ script.js
@@ -4,3 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const currentYear = new Date().getFullYear();
     yearElement.textContent = currentYear;
 });
+
+// Find out the variable "personId" is not equal to variable "jsonPersonIdParse" that is "personId" variable after retrieving its content for JSON
+/* const personId = {
+    name: "Armel",
+    age: 25,
+    career: {
+        firstCareer: "Developer",
+        secondCareer: "Nurse",
+        thirdCareer: "Musician",
+        fourthCareer: "Preacher"
+    }
+} */
+
+// Learn the difference between "if statement" and "switch" statement
+
+// While loop
+
